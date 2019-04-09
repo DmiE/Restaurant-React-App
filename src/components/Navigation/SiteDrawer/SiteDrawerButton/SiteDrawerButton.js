ab84efd30b6e1dc03beabf62de350b788424949c
DmiE <Dawid.Mieszczak95@gmail.com>
2019-04-09 10:49:56 +0200

    add styling to SiteDrawerButton

--- src/components/Navigation/SiteDrawer/SiteDrawerButton/SiteDrawerButton.js
+++ src/components/Navigation/SiteDrawer/SiteDrawerButton/SiteDrawerButton.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import classes from './SiteDrawerButton.css'
+
 
 const siteDrawerButton = (props) => (
-    <div onClick={props.clicked}>MENU</div>
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 );
 
 export default siteDrawerButton;
