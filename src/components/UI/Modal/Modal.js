44b0e2804133df6c96c5ad939a3fab718b09eee9
DmiE <Dawid.Mieszczak95@gmail.com>
2019-04-09 10:38:28 +0200

    change Modal component to class component and check if it should to be rerender

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.css'
 import ReactAux from '../../../hoc/ReactAux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <ReactAux>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-            {props.children}
-        </div>
-    </ReactAux>
-);
+class Modal extends Component {
+    
+    shouldComponentUpdate ( nextProps, nextState ) {
+        return nextProps.show !== this.props.show;
+    }
 
-export default modal;
+    componentWillUpdate () {
+        console.log('[Modal] WillUpdate');
+    }
+    
+    render() {
+        return (
+            <ReactAux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </ReactAux>
+        )
+    }
+} 
+
+export default Modal;
