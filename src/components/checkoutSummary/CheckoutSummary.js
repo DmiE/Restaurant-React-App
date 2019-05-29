0bdfbcf94098bbd2fda2b20563ce02cea57d32a7
DmiE <Dawid.Mieszczak95@gmail.com>
2019-05-29 17:39:28 +0200

    add checkout component

---
+++ src/components/checkoutSummary/CheckoutSummary.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Burger from '../Burger/Burger'
+import Button from '../UI/Button/Button'
+import classes from './CheckoutSummary.css'
+
+const checkoutSummary = (props) => {
+
+    return (
+        <div className={classes.CheckoutSummary}>
+            <div style={{width: '100%', margin: 'auto'}}>
+            <Burger ingredients={props.ingredients} />
+            </div>
+            <Button btnType='Danger' clicked>CANCEL</Button>
+            <Button btnType='Succes' clicked>CONTINUE</Button>
+        </div>
+    )
+} 
+
+export default checkoutSummary;
